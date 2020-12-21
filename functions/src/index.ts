b7df431aa00845a1a3f1624d2ba1b702555444b2
SuzukiKensuke <s1811349@s.tsukuba.ac.jp>
2020-12-21 11:19:29 +0900

    add rating function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,50 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import axios from "axios";
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+export const rating = functions.https.onRequest(
+  async (request, response) => {
+    const { user } = request.query;
+
+    if (user == undefined){
+      response.status(400).send({ message: "set your username" });
+      return;
+    }
+
+    try {
+      const { data } = await axios.get<Record[]>(
+        `https://atcoder.jp/users/${user}/history/json`
+      );
+
+      if (data.length === 0){
+        response.status(400).send({ message: "no such user or no contest record" });
+        return;
+      }
+
+      const latestRecord = data[data.length - 1];
+      const { NewRating } = latestRecord;
+
+      response.status(200).send({ message: "success", rating: `${NewRating}` });
+      return;
+    } catch (e) {
+      response.status(503).send({ message: "server error" });
+      return;
+    }
+  }
+);
+
+interface Record {
+  IsRated: boolean;
+  Place: number;
+  OldRating: number;
+  NewRating: number;
+  Performance: number;
+  InnerPerformance: number;
+  ContestScreenName: string;
+  ContestNameEn: string;
+  EndTime: string;
+}
+
+// interface Result {
+//   message: string;
+//   rating?: number;
+// }
